c0fd9a834c304a9c3882a23f651b35b21545520c
BogdanAziev2003 <neazievbogdan@gmail.com>
2023-12-12 21:35:03 +0300

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const TelegramBot = require("node-telegram-bot-api");
+require("dotenv").config();
+
+const token = process.env.API_KEY;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(
+    chatId,
+    `Бот переехал!!!
+Текущий адресс: @goodfood_bsln_bot`
+  );
+});
